9c554ce5c5f86102076b7dba40c91effc64bc215
lealivan009 <98489600+lealivan009@users.noreply.github.com>
2024-08-05 23:34:11 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gim_frontend_react',
+  webDir: 'dist'
+};
+
+export default config;
